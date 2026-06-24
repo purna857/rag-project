129e2f93e9c23b06b1ef86af5f4e8e7371ebf0dd
purna857 <purnachandrasekharb629@gmail.com>
2026-06-24 19:32:03 +0530

    Complete Python production setup

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv 
-import os 
+from dotenv import load_dotenv
+import os
 
-load_dotenv()  # Load environment variables from .env file 
-APP_NAME = os.getenv("APP_NAME")  # Default to "MyApp" if not set 
+load_dotenv()  # Load environment variables from .env file
+APP_NAME = os.getenv("APP_NAME")  # Default to "MyApp" if not set
--- src/services/app_service.py
+++ src/services/app_service.py
@@ -1,13 +1,16 @@
-from venv import logger
 from config.settings import APP_NAME
-from utils.logger import logger 
+from utils.logger import logger
+
+
 def start_application():
-    logger.info("RAG application has started.")  # Log when the application starts
+    logger.info("RAG application has started.")
 
     try:
-        number = 10/0  # This will raise a ZeroDivisionError
+        10 / 0  # This will raise a ZeroDivisionError
     except Exception as e:
-        logger.error(f"An error occurred: {e}")  # Log the error message 
+        logger.error(f"An error occurred: {e}")
 
     finally:
-        logger.info(f"{APP_NAME} application has stopped.")  # Log when the application stops
+        logger.info(
+            f"{APP_NAME} application has stopped."
+        )  # Log when the application stops
